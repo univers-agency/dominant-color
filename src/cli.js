d19f60c519453d6bda4bdd23aaedd6a785fdb913
T. Mjoen <trond@twined.net>
2021-12-21 11:33:15 +0100

    Improve rgbToHex

--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,13 @@
 import sharp from "sharp";
 
-function componentToHex(c) {
-  var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+function rgbToHex(red, green, blue) {
+  return `#${(blue | (green << 8) | (red << 16) | (1 << 24))
+    .toString(16)
+    .slice(1)}`
 }
 
 export async function cli(args) {
   const image = process.argv.slice(2).join(" ");
   const { dominant } = await sharp(image).stats();
-  const { r, g, b } = dominant;
-  console.log(rgbToHex(r, g, b));
+  console.log(rgbToHex(dominant.r, dominant.g, dominant.b));
 }
